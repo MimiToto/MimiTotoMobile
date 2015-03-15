76a56a75df87ac98daff289f206264762467ff42
jgbs <joaogabrieldebrittoesilva@gmail.com>
2015-03-14 23:54:08 -0300

    Adding test button with Toast

--- app/src/main/java/br/mimitoto/mimitotomobile/MainActivity.java
+++ app/src/main/java/br/mimitoto/mimitotomobile/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,6 +16,9 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void buttonTest(View view){
+        Toast.makeText(this, "Olá mundo Android!", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
